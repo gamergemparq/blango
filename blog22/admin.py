c882c9e94af3da6fadaf365c5601ec4987467015
Hazrat Muslimen <muhammad.hazrat.muslimen@rhbgroup.com>
2022-08-18 08:42:59 +0000

    Finish generic relationships

--- blog22/admin.py
+++ blog22/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Tag, Post, Comment
+from blog.models import Tag, Post
 
 class PostAdmin(admin.ModelAdmin):
   prepopulated_fields = {"slug": ("title",)}
@@ -8,4 +8,3 @@ class PostAdmin(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
-admin.site.register(Comment)
