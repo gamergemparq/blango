9ef44bf19e5a6d58c1c3871153c160cf7dd5b76e
Hazrat Muslimen <muhammad.hazrat.muslimen@rhbgroup.com>
2022-08-19 12:05:30 +0000

    Finish custom template tags

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from blog.models import Post
 
@@ -8,3 +8,8 @@ def index(request):
   posts = Post.objects.filter(published_at__lte=timezone.now())
   context['posts'] = posts
   return render(request, "blog/index.html", context)
+
+def post_detail(request, slug):
+  post = get_object_or_404(Post, slug=slug)
+  context = {"post": post}
+  return render(request, "blog/post-detail.html", context)
